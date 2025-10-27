6ef7c2acd0faaed6c2cd34d1084f54e0920cc926
Dmytro Horbatko <kefir266@yahoo.com>
2025-10-27 14:56:58 +0100

    added streams

--- services/binance.service.ts
+++ services/binance.service.ts
@@ -1,9 +1,15 @@
 import dotenv from "dotenv";
-import { Spot, SpotRestAPI, SPOT_WS_STREAMS_PROD_URL } from "@binance/spot";
+import {
+  Spot,
+  SpotRestAPI,
+  SPOT_WS_STREAMS_PROD_URL,
+  SpotWebsocketStreams,
+} from "@binance/spot";
 dotenv.config();
 
 class BinanceService {
   connection: any;
+  websocketConnection;
 
   constructor() {
     this.connection = new Spot({
@@ -12,12 +18,27 @@ class BinanceService {
         apiSecret: process.env.APISECRET,
       },
     });
+
+    this.websocketConnection = new Spot({
+      configurationWebsocketStreams: {
+        wsURL: SPOT_WS_STREAMS_PROD_URL,
+      },
+    });
   }
 
   async get(symbol: string) {
     const response = await this.connection.restAPI.exchangeInfo({ symbol });
 
     return response.data();
+  }
+
+  getStreamData(onDataFn: any, symbol: string) {
+    this.websocketConnection.websocketStreams
+      .connect()
+      .then((connection: SpotWebsocketStreams.WebsocketStreamsConnection) => {
+        const stream = connection.aggTrade({ symbol });
+        stream.on("message", onDataFn);
+      });
   }
 }
 
